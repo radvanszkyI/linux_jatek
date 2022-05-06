3a8303e82fce167b5b36664ba8dda48658d45305
radvanszkyI <45069028+radvanszkyI@users.noreply.github.com>
2022-05-06 08:11:51 +0200

    v
    2x map
    hero

--- 2file.sh
+++ 2file.sh
@@ -2,16 +2,17 @@
 
 border_color="\e[30;43m"
 green="\e[32;42m"
-blue="\e[34;44m"
+blue="\e[0;44m"
 text_color="\e[31;43m"
 no_color="\e[0m"
 WALL='\u253C'
 
 declare -A matrix
-input="./tar.txt"
-N=7
-M=8
+input="./map.txt"
+N=16
+M=24
 
+#ok
 draw() { # X, Y, char, bg-color 
 	if [ $# -lt 0 ]
 	  then
@@ -30,8 +31,9 @@ draw() { # X, Y, char, bg-color
 	fi
 } # pl.: draw 3 20 "-" $green
 
-
+#ok
 load() {
+	i=0
     	while IFS= read -r line
     	do
     	    #echo $i
@@ -43,13 +45,18 @@ load() {
     	done < "$input"
 }
 
-#todo
+#ok
 print_matrix() {
 	for ((i=0;i<N;i++))
 	do
 	    for ((j=0;j<M;j++))
 	    do
-		    echo -n "${matrix[$i,$j]}_"
+	    	    ((x=8+i))
+	    	    ((y=20+j*2))
+	    	    c=${matrix[$i,$j]}
+	    	    #echo -n "$x $y $c "
+	    	    
+		    draw $x $y $c$c $border_color
 	    done
 	    echo
 	done
@@ -57,12 +64,14 @@ print_matrix() {
 }
 
 
-n=2;m=4
+#type here your 2 letter monogram or just press enter(m="ME")
 
-i=0
-
-
+#ok
 load	
 #echo ${matrix[1,2]} # ez oké
-print_matrix 
 
+print_matrix 
+draw 9 22 "ME" $blue
+
+#echo ${matrix[0,0]}
+draw 30 1 "\n" $no_color #end of game
